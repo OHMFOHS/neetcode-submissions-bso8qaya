a5ddaaea36dcf7f89386d73853b523a2271dad62
Junchao Mao <52117946+OHMFOHS@users.noreply.github.com>
2026-06-02 17:47:38 -0700

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<Character> path = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        dfs(n, path, ans, 0, 0);
+        return ans;
+
+    }
+
+    void dfs(int n, List<Character> path, List<String> ans, int left, int right) {
+        if (path.size() == n * 2) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n * 2; i++) {
+                sb.append(path.get(i));
+            }
+            ans.add(sb.toString());
+            return;
+        }
+        
+        //用 if 两种可能都尝试一下
+        if(left < n) {
+            path.add('(');
+            dfs(n, path, ans, left + 1, right);
+            path.remove(path.size() - 1);
+        }
+
+        if(left > right && right < n) {
+            path.add(')');
+            dfs(n, path, ans, left, right + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
