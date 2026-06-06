47992fe187e37410243268ac2a949b068222db8a
Junchao Mao <52117946+OHMFOHS@users.noreply.github.com>
2026-06-06 13:24:50 -0700

    Add: non-overlapping-intervals - submission-4

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        return intervals.length - dfs(0, -1, intervals);
+    }
+
+    private int dfs(int i, int prev, int[][] intervals) {
+        if (i == intervals.length) {
+            return 0;
+        }
+        //不选
+        int res = dfs(i + 1, prev, intervals);
+        //选
+        //注意如果选的话， 记得+1， 代表能保留下来的结果 + 1
+        if (intervals[i][0] >= prev || prev == -1) {
+            res = Math.max(res, 1 + dfs(i + 1, intervals[i][1], intervals));
+        }
+        return res;
+    }
+}
